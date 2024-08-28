9b75a6924c8bac6ee77c0a67ca9d13e27d32552d
wingedlionn <76070123+wingedlionn@users.noreply.github.com>
2024-08-28 10:35:33 +0100

    updated program.py & readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-heavily uses code from https://github.com/ArmindoFlores/ao3_api
+# Note: This uses code from https://github.com/ArmindoFlores/ao3_api
+
+## What is this?
+Some scripts to download fanworks from Archive Of Our Own. It utilises ArminoFlores' ao3_api & reuses functionality from their codebase.
+
+## How do I use this?
+Both scripts contain a "search" parameter that can be changed by the user to return a variety of results. I have left an example of a search term in each script. More examples can be found in the ao3_api docs & codebase.
+### Manually:
+After editing the search param and running the program, you will be prompted to download each work with its title, author, & other relevant info incl. tags. These can be skipped or downloaded. 
+### Automatically:
+Using the autodl.py script, you can download each work without any user input. 
+*** Note that this mode should be monitored as the program could crash due to problems out of my control (rate limits etc) ***
+
--- program.py
+++ program.py
@@ -1,12 +1,18 @@
 import AO3 
-import bs4
+import math
 from AO3 import works, utils, search, GuestSession, Session, common
 
-results = AO3.search.search(relationships="Dande | Leon/Sonia", sort_column="kudos_count", word_count=AO3.utils.Constraint(0, 50000), completion_status="complete")
+forceExit = False
+pageNum = 1
 
-def searchFics():
+
+def getResults(pageNum):
+    results = AO3.search.search(relationships="Dande | Leon/Sonia", sort_column="kudos_count", word_count=AO3.utils.Constraint(0, 50000), completion_status="complete", page=pageNum)
+    return results
+
+def getFics(results):
+    global forceExit
     idList = []
-
     for work in results.find_all("li", {"role": "article"}):
         if work.h4 is None:
             continue
@@ -17,21 +23,13 @@ def searchFics():
                     if "author" in a['rel']:
                         continue
                 elif a.attrs["href"].startswith("/works"):
-                    workname = a.string
                     workid = utils.workid_from_url(a['href'])
                     idList.append(workid)
         except AttributeError:
             pass
 
-    maindiv = results.find("div", {"class": "works-search region", "id": "main"})
-    total_results = int(maindiv.find("h3", {"class": "heading"}).getText().replace(',','').replace('.','').strip().split(" ")[0])
-       
-
-    print(f"Results found: {total_results}\nResults shown: {len(idList)}")
-
-    forceExit = False
-
     for id in idList:
+        print(id)
         work = AO3.Work(id)
         print(f"Do you want to download {work.title} by {work.authors}?\nIt was updated on {work.date_updated}, has {work.kudos} kudos and {work.words} words.\n")
         print(f"Relationships: {work.relationships}\nCharacters: {work.characters}\nTags: {work.tags}\n")
@@ -47,10 +45,52 @@ def searchFics():
         else:
             print("Work skipped.\n")
 
-    if forceExit == False:
-        print("End of results on selected page.")
 
-        
+
+
+
+def searchFics():
+    global forceExit, pageNum
+    results = getResults(1)
+    idList = []
+
+    for work in results.find_all("li", {"role": "article"}):
+        if work.h4 is None:
+            continue
+
+        try:
+            for a in work.h4.find_all("a"):
+                if 'rel' in a.attrs.keys():
+                    if "author" in a['rel']:
+                        continue
+                elif a.attrs["href"].startswith("/works"):
+                    workid = utils.workid_from_url(a['href'])
+                    idList.append(workid)
+        except AttributeError:
+            pass
+
+    maindiv = results.find("div", {"class": "works-search region", "id": "main"})
+    total_results = int(maindiv.find("h3", {"class": "heading"}).getText().replace(',','').replace('.','').strip().split(" ")[0])
+    
+    numPages = math.ceil(total_results / 20)
+
+
+    print(f"Results found: {total_results} over {numPages} page(s).\nResults shown: {len(idList)}")
+    
+    getFics(results)
+
+    while forceExit == False:
+        print("End of results on selected page.")
+        if(numPages>1):
+            res = input(f"Do you want to view the next page?\ny/n")
+            if res == "y":
+                pageNum += 1
+                results = getResults(pageNum)
+                getFics(results)
+            else:
+                forceExit = True
+
+    print("complete!")
 
 
 def main():
